8572fe435185e137c5e7335ca795d56b036730be
Sarbajit Nandy <sarbajitnandey@gmail.com>
2024-12-19 11:57:02 +0530

    Time: 0 ms (100%), Space: 9.2 MB (20.72%) - LeetHub

--- 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
@@ -3,17 +3,13 @@ public:
     int maxChunksToSorted(vector<int>& arr) {
         int chunk=0, n= arr.size();
 
-        int left = 0,right=0;
-        int i = 0, c = 0;
+        int mx = -1;
         for(int i=0;i<n;i++) {
-            if (!(left<=arr[i] && arr[i]<=right)) {
-                if (right - left + 1 == c) {chunk++; c=1;left=i; right = arr[i];}
-                else {right = arr[i]; c++;}
-            } else c++;
+            mx = max(mx, arr[i]);
+            if (i==mx) {
+                chunk++; mx = -1;                           
+            }
         }
-
-        if (arr[n-1]==n-1 || left!=right) chunk++; 
-
         return chunk;
     }
 };
