1d6b519e03342b0e14519a356e9d42a182f6476c
Sarbajit Nandy <sarbajitnandey@gmail.com>
2025-01-13 23:45:36 +0530

    Time: 2 ms (46.89%), Space: 10.5 MB (64.31%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
-
-    void recur(int index, vector<int>& nums, vector<int>& temp, vector<vector<int>>& res) {
+    void recur(int index, vector<int>& nums, vector<int>& temp,
+               vector<vector<int>>& res) {
         if (index == nums.size()) {
             res.push_back(temp);
             return;
         }
+        temp.push_back(nums[index]);
+        recur(index + 1, nums, temp, res);
+        temp.pop_back();
 
-        // for(int i=index;i<nums.size();i++) {
-        //     if (i!=index && nums[i]==nums[index]) continue;
-            temp.push_back(nums[index]);
-            recur(index+1, nums,temp, res);
-            temp.pop_back();
-
-            while(index<nums.size()-1 && nums[index+1]==nums[index]) index++;
-            recur(index+1, nums,temp, res);
-
-        // }
-    } 
+        while (index < nums.size() - 1 && nums[index + 1] == nums[index])
+            index++;
+        recur(index + 1, nums, temp, res);
+    }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(), nums.end());
 
         vector<int> temp;
         vector<vector<int>> res;
 
-        recur(0,nums, temp, res);
-        
+        recur(0, nums, temp, res);
+
         return res;
     }
 };
