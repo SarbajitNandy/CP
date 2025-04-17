37d6c41eca9d4ec7c58ceee51914c949f67918ed
Sarbajit Nandy <sarbajitnandey@gmail.com>
2025-04-17 18:31:07 +0530

    Time: 47 ms (75.03%), Space: 13.6 MB (61.88%) - LeetHub

---
+++ 0277-find-the-celebrity/0277-find-the-celebrity.cpp
@@ -0,0 +1,26 @@
+/* The knows API is defined for you.
+      bool knows(int a, int b); */
+
+class Solution {
+public:
+    int findCelebrity(int n) {
+        int N = n;
+    int candidate = 0;
+
+    for (int i = 1; i < N; ++i) {
+        if (knows(candidate, i)) {
+            candidate = i;
+        }
+    }
+
+    for (int i = 0; i < N; ++i) {
+        if (i != candidate) {
+            if (!knows(i, candidate) || knows(candidate, i)) {
+                return -1;
+            }
+        }
+    }
+
+    return candidate;
+    }
+};
