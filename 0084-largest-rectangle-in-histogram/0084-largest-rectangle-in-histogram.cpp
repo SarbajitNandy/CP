3f7a8d120a0ee7469df735b1a369030d338afb12
Sarbajit Nandy <sarbajitnandey@gmail.com>
2025-04-21 23:48:55 +0530

    Time: 14 ms (89.38%), Space: 85.7 MB (36.89%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -3,17 +3,17 @@ private:
     vector<int> findNSE(vector<int>& h ) {
         int n = h.size();
         stack<int> st;
-        vector<int> nse(n,-1);
-        st.push(h.size()-1);
+        vector<int> nse(n,0);
+        // st.push(h.size()-1);
 
-        for(int i=n-2;i>=0;i--) {
+        for(int i=n-1;i>=0;i--) {
             while(!st.empty() && h[st.top()]>=h[i]) {
                 st.pop();
             }
 
-            if (!st.empty()) {
-                nse[i]=st.top();
-            }
+            // if (!st.empty()) {
+                nse[i]=st.empty()? n : st.top();
+            // }
             st.push(i);
         } 
 
@@ -29,17 +29,17 @@ private:
     vector<int> findPSE(vector<int>& h ) {
         int n = h.size();
         stack<int> st;
-        vector<int> pse(h.size(),-1);
-        st.push(0);
+        vector<int> pse(h.size(),0);
+        // st.push(0);
 
-        for(int i=1;i<n;i++) {
+        for(int i=0;i<n;i++) {
             while(!st.empty() && h[st.top()]>=h[i]) {
                 st.pop();
             }
 
-            if (!st.empty()) {
-                pse[i]=st.top();
-            }
+            // if (!st.empty()) {
+                pse[i]=st.empty() ? -1 : st.top();
+            // }
             st.push(i);
         } 
 
@@ -53,23 +53,16 @@ private:
     }
 public:
     int largestRectangleArea(vector<int>& heights) {
+        if (heights.size()==1) return heights[0];
         auto pse = findPSE(heights);
         auto nse = findNSE(heights);
         int n = heights.size();
         int res = 0;
 
         for(int i=0;i<heights.size();i++) {
-            res = max(res, ((nse[i]==-1 ? n : nse[i])-pse[i]-1)*heights[i]);
+            res = max(res, (nse[i]-pse[i]-1)*heights[i]);
         }
 
         return res;
     }
 };
-
-/*
-2,1,5,6,2,3
-1  -1  2  2  -1  -1
-
--1 -1 1 5 1 2
-
-*/
